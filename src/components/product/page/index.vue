f7d72477366fb3d03f913287d23e585245def35b
BlockHead <1955372313@qq.com>
2020-03-22 17:17:44 +0800

    ss

--- src/components/product/page/index.vue
+++ src/components/product/page/index.vue
@@ -1,6 +1,17 @@
 <template>
-	<div>
-		
+	<div class="user">
+		<!-- <el-row>
+			<el-col :xs="6" :sm="6" :md="6" :lg="6" :xl="6">
+				<div class="nvaLeft">
+					<svg width="12" height="10.56" class="_3laxbDuw"><g><path d="M4.44 4.23h7.2a.12.12 0 0 0 .12-.12v-.84a.12.12 0 0 0-.12-.12h-7.2a.12.12 0 0 0-.12.12v.84a.12.12 0 0 0 .12.12zm-.12 3.06a.12.12 0 0 0 .12.12h7.2a.12.12 0 0 0 .12-.12v-.84a.12.12 0 0 0-.12-.12h-7.2a.12.12 0 0 0-.12.12zM11.88 0H.12A.12.12 0 0 0 0 .12v.84a.12.12 0 0 0 .12.12h11.76A.12.12 0 0 0 12 .96V.12a.12.12 0 0 0-.12-.12zm0 9.48H.12A.12.12 0 0 0 0 9.6v.84a.12.12 0 0 0 .12.12h11.76a.12.12 0 0 0 .12-.12V9.6a.12.12 0 0 0-.12-.12zM.051 5.38l2.345 1.846a.133.133 0 0 0 .216-.1V3.433a.133.133 0 0 0-.216-.1L.051 5.179a.131.131 0 0 0 0 .204z" fill="#e8edee"></path><path d="M.051 5.383l2.345 1.846a.133.133 0 0 0 .216-.1V3.436a.133.133 0 0 0-.216-.1L.051 5.182a.131.131 0 0 0 0 .201z" fill="#ff7800"></path></g></svg>
+					<span>抖音数据平台</span>
+				</div>
+				
+			</el-col>
+			<el-col :xs="18" :sm="18" :md="18" :lg="18" :xl="18">
+				<input type="text">
+			</el-col>
+		</el-row> -->
 	</div>
 </template>
 
@@ -27,7 +38,7 @@ export default {
 
   },
   beforeRouteLeave(to, from, next) {
-  	let scrollTop = this.scrollTop =document.getElementById('hospital').scrollTop;
+  	let scrollTop = this.scrollTop =document.getElementById('productPage').scrollTop;
   	this.scrollTop = scrollTop?scrollTop :0;
   	console.log(this.scrollTop)
   	if(!to.query.time || !from.query.time || to.query.time < from.query.time){
@@ -63,7 +74,7 @@ export default {
 	//进入该页面时，用之前保存的滚动位置赋值
 	beforeRouteEnter(to, from, next) {
 		next(vm => {
-			document.getElementById('hospital').scrollTop=document.getElementById('hospital').pageYOffset=vm.scrollTop;
+			document.getElementById('productPage').scrollTop=document.getElementById('productPage').pageYOffset=vm.scrollTop;
 		});
 		//判断回调页是否与当前前进路由相等，如果相等，删除路径
 		let fromRoute =  JSON.stringify({path:from.path,name:from.name,query:from.query})
