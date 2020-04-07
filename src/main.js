84a72478b681c50732cb703f9fec0c6ed58a1f29
onerunsall <413038044@qq.com>
2020-04-07 15:00:03 +0800

    pack

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 // import './assets/rest.css'
-
+ 
 import App from './App'
 import router from './router'
 import axios from 'axios'
