b45ed78f4721cafbdf4610b3d9b23432cdeea6bc
onerunsall <413038044@qq.com>
2020-04-07 16:34:11 +0800

    1

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ Vue.use(VueLazyload, {
 
 
  
-Vue.prototype.$version = '2004071609-39af44b'
+Vue.prototype.$version = '2004071633-d8e85f9'
 Vue.prototype.$versionBrief = '修复了已知BUG, 优化了用户体验.'
 Vue.config.productionTip = false
 
