5bb91760134ea05faf77fe89bba312ecf476e07c
BlockHead <1955372313@qq.com>
2020-04-10 17:31:48 +0800

    s

--- src/components/product/collection/videoAccount.vue
+++ src/components/product/collection/videoAccount.vue
@@ -22,7 +22,7 @@
 						<input @click="searchColor=true" type="search" @keydown.enter="searchFn" v-model="kw" placeholder="请输入关键字">
 						<svg v-if="kw" @click="kw =''" viewBox="64 64 896 896" focusable="false" class="" data-icon="close-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M685.4 354.8c0-4.4-3.6-8-8-8l-66 .3L512 465.6l-99.3-118.4-66.1-.3c-4.4 0-8 3.5-8 8 0 1.9.7 3.7 1.9 5.2l130.1 155L340.5 670a8.32 8.32 0 0 0-1.9 5.2c0 4.4 3.6 8 8 8l66.1-.3L512 564.4l99.3 118.4 66 .3c4.4 0 8-3.5 8-8 0-1.9-.7-3.7-1.9-5.2L553.5 515l130.1-155c1.2-1.4 1.8-3.3 1.8-5.2z"></path><path d="M512 65C264.6 65 64 265.6 64 513s200.6 448 448 448 448-200.6 448-448S759.4 65 512 65zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path></svg>
 					</div>
-					<span @click="searchFn" :class="searchColor? 'searchColor':''">搜索</span>
+					<span @click="searchFn" :class="searchColor? 'searchColor':''">搜 索</span>
 				</div>
 				<div v-if='show_jieguo[0]||show_jieguo[1]' class="ant-col" style="line-height: 60px;padding-left: 16px; padding-right: 16px; float: left;">
 					<span class="_3EYKFwfn">{{searchNum}}</span>
